3e5221b5bc58abd616e6f39e751287a7213b79c2
Merlijn Sebrechts <merlijn.sebrechts@gmail.com>
2022-09-10 10:26:05 +0200

    quit on error and ignore build lock

--- publish.sh
+++ publish.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -e
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo 
 
 # Go To Public folder
 cd public
